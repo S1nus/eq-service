e73f040ce8ef527556fdfeafb46e5ac372ce2ff9
c-node <chuckhead7@gmail.com>
2025-01-13 23:30:59 -0500

    move code to helper function for reusage

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ use nmt_rs::{
 };
 
 mod utils;
-use utils::create_header_field_tree;
+use utils::{create_inclusion_proof_input, InclusionProofInput};
 
 // Using rkyv for serialization
 #[derive(Archive, RkyvDeserialize, RkyvSerialize)]
@@ -60,63 +60,14 @@ impl Inclusion for InclusionService {
         // Get the ExtendedHeader
         let header = self.client.header_get_by_height(height)
             .await
-            .map_err(|e| Status::internal(e.to_string()))?;
-
-        let eds_row_roots = header.dah.row_roots();
-        let eds_column_roots = header.dah.column_roots();
-    
-        // Compute these values needed for proving inclusion
-        let eds_size: u64 = eds_row_roots.len().try_into().unwrap();
-        let ods_size = eds_size / 2;
+            .map_err(|e| Status::internal(format!("Failed to get header: {}", e.to_string())))?;
 
         let nmt_multiproofs = self.client
             .blob_get_proof(height, namespace, commitment)
             .await
-            .map_err(|e| Status::internal(format!("Failed to get blob proof: {}", e)))?;
+            .map_err(|e| Status::internal(format!("Failed to get blob proof: {}", e.to_string())))?;
 
-        let (mut header_field_tree, _data_hash_proof) = create_header_field_tree(&header);
-        let data_hash_from_tree = TmHash::decode_vec(&header_field_tree.root())
-            .map_err(|e| Status::internal(format!("Failed to decode data hash: {}", e)))?;
-
-        let blob_index = blob.index.ok_or_else(|| Status::internal("Blob index not found"))?;
-        let blob_size: u64 = max(1, blob.to_shares().unwrap().len() as u64);
-        let first_row_index: u64 = blob_index.div_ceil(eds_size) - 1;
-        let ods_index = blob_index - (first_row_index * ods_size);
-
-        let last_row_index: u64 = (ods_index + blob_size).div_ceil(ods_size) - 1;
-
-        let hasher = TmSha2Hasher {};
-        let mut row_root_tree: MerkleTree<MemDb<[u8; 32]>, TmSha2Hasher> =
-            MerkleTree::with_hasher(hasher);
-    
-        let leaves = eds_row_roots
-            .iter()
-            .chain(eds_column_roots.iter())
-            .map(|root| root.to_array())
-            .collect::<Vec<[u8; 90]>>();
-    
-        for root in &leaves {
-            row_root_tree.push_raw_leaf(root);
-        }
-    
-        // assert that the row root tree equals the data hash
-        assert_eq!(row_root_tree.root(), data_hash_from_tree.as_bytes());
-        // Get range proof of the row roots spanned by the blob
-        // +1 is so we include the last row root
-        let row_root_multiproof =
-            row_root_tree.build_range_proof(first_row_index as usize..(last_row_index + 1) as usize);
-        // Sanity check, verify the row root range proof
-        let hasher = TmSha2Hasher {};
-        let leaves_hashed = leaves
-            .iter()
-            .map(|leaf| hasher.hash_leaf(leaf))
-            .collect::<Vec<[u8; 32]>>();
-        row_root_multiproof
-            .verify_range(
-                data_hash_from_tree.as_bytes().try_into().unwrap(),
-                &leaves_hashed[first_row_index as usize..(last_row_index + 1) as usize],
-            )
-            .map_err(|_| Status::internal("Failed sanity check on row root inclusion multiproof"))?;
+        let inclusion_proof_input = create_inclusion_proof_input(&blob, &header, nmt_multiproofs)?;
 
         Ok(Response::new(GetKeccakInclusionResponse { status: 0 }))
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use tonic::Status;
 use nmt_rs::{
     simple_merkle::{db::MemDb, proof::Proof, tree::{MerkleTree, MerkleHash}},
     TmSha2Hasher,
@@ -7,18 +8,70 @@ use tendermint_proto::{
     v0_37::{types::BlockId as RawBlockId, version::Consensus as RawConsensusVersion},
     Protobuf,
 };
-use celestia_types::nmt::NamespaceProof;
+use celestia_types::{nmt::{NamespaceProof, NamespacedHashExt}, blob::Blob, ExtendedHeader};
+use std::cmp::max;
 
-pub struct InclusionProofData {
+pub struct InclusionProofInput {
     pub nmt_multiproofs: Vec<NamespaceProof>,
     pub row_root_multiproof: Proof<TmSha2Hasher>,
 }
 
-pub fn create_inclusion_proof_input() -> Result<InclusionProofData> {
+pub fn create_inclusion_proof_input(blob: &Blob, header: &ExtendedHeader, nmt_multiproofs: Vec<NamespaceProof>) -> Result<InclusionProofInput, Status> {
 
+    let eds_row_roots = header.dah.row_roots();
+    let eds_column_roots = header.dah.column_roots();
+
+    // Compute these values needed for proving inclusion
+    let eds_size: u64 = eds_row_roots.len().try_into().unwrap();
+    let ods_size = eds_size / 2;
+
+    let blob_index = blob.index.ok_or_else(|| Status::internal("Blob index not found"))?;
+    let blob_size: u64 = max(1, blob.to_shares().unwrap().len() as u64);
+    let first_row_index: u64 = blob_index.div_ceil(eds_size) - 1;
+    let ods_index = blob_index - (first_row_index * ods_size);
+
+    let last_row_index: u64 = (ods_index + blob_size).div_ceil(ods_size) - 1;
+
+    let hasher = TmSha2Hasher {};
+    let mut row_root_tree: MerkleTree<MemDb<[u8; 32]>, TmSha2Hasher> =
+        MerkleTree::with_hasher(hasher);
+
+    let leaves = eds_row_roots
+        .iter()
+        .chain(eds_column_roots.iter())
+        .map(|root| root.to_array())
+        .collect::<Vec<[u8; 90]>>();
+
+    for root in &leaves {
+        row_root_tree.push_raw_leaf(root);
+    }
+
+    // assert that the row root tree equals the data hash
+    assert_eq!(row_root_tree.root(), header.header.data_hash.unwrap().as_bytes());
+    // Get range proof of the row roots spanned by the blob
+    // +1 is so we include the last row root
+    let row_root_multiproof =
+        row_root_tree.build_range_proof(first_row_index as usize..(last_row_index + 1) as usize);
+    // Sanity check, verify the row root range proof
+    let hasher = TmSha2Hasher {};
+    let leaves_hashed = leaves
+        .iter()
+        .map(|leaf| hasher.hash_leaf(leaf))
+        .collect::<Vec<[u8; 32]>>();
+    row_root_multiproof
+        .verify_range(
+            header.header.data_hash.unwrap().as_bytes().try_into().unwrap(),
+            &leaves_hashed[first_row_index as usize..(last_row_index + 1) as usize],
+        )
+        .map_err(|_| Status::internal("Failed sanity check on row root inclusion multiproof"))?;
+
+    Ok(InclusionProofInput {
+        nmt_multiproofs,
+        row_root_multiproof,
+    })
 }
 
-pub fn create_header_field_tree(header: &celestia_types::ExtendedHeader) -> (MerkleTree<MemDb<[u8; 32]>, TmSha2Hasher>, Proof<TmSha2Hasher>) {
+pub fn create_header_field_tree(header: &ExtendedHeader) -> (MerkleTree<MemDb<[u8; 32]>, TmSha2Hasher>, Proof<TmSha2Hasher>) {
     let hasher = TmSha2Hasher {};
     let mut header_field_tree: MerkleTree<MemDb<[u8; 32]>, TmSha2Hasher> =
         MerkleTree::with_hasher(hasher);
