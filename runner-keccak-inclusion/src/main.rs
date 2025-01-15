76e5a74db33222eb886eeb4af0b24aaeee27daf6
c-node <chuckhead7@gmail.com>
2025-01-15 16:32:26 -0500

    runner for keccak

---
+++ runner-keccak-inclusion/src/main.rs
@@ -0,0 +1,6 @@
+use sp1_sdk::{SP1Stdin, ProverClient};
+
+const KECCAK_INCLUSION_ELF: &[u8] = include_bytes!("../target/elf-compilation/riscv32im-succinct-zkvm-elf/release/eq-program-keccak-inclusion");
+
+fn main() {
+}
